1826dd7a1e0daae5a13ffd4e0bc357be39b49a3f
mukisa-emmanuel <mukisae44@gmail.com>
2025-09-23 20:27:31 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+// Simple interactivity: Alert contact info when clicking the contact section
+document.getElementById('contact').addEventListener('click', function() {
+    alert('Contact: mukisae44@gmail.com\nGitHub: https://github.com/mukisa001Emmanuel/Portfolio/tree/main');
+});
